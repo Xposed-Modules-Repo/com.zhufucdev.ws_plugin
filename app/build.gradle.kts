bef74436de6af4e86f31bedbbf1203610c6a3e03
zhufucdev <1095291434@qq.com>
2024-01-01 08:30:34 +0800

    :rocket: Make it compile

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,9 @@ android {
             )
         }
     }
+    buildFeatures {
+        buildConfig = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
